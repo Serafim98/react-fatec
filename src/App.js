e94d054f137d881a77492a22f989f1decee8a561
Luis Serafim <fernandodss30@hotmail.com>
2022-09-26 22:02:01 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Vestibular Fatec 2022</h1>
+       <h2>Ensino Superior</h2>
+       <p>Cursos <b>GRATUITOS</b> e de Qualidade</p>
+       <p>Presenciais e a Distância</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
